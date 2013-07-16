0242399beb726d58f5986429b2a07996982ee98e
Stephanie Datu <stephdatu@gmail.com>
2013-07-16 12:57:14 -0700

    Add title functionality for show page

--- spec/integration/creating_entries_spec.rb
+++ spec/integration/creating_entries_spec.rb
@@ -4,12 +4,17 @@ feature 'Creating Entries' do
   scenario "can create an entry" do
     visit '/'
     click_link 'New Entry'
-    #select '2013', :from => 'entry[calendar_date(1i)]'
-    #select 'July', :from => 'entry[calendar_date(2i)]'
-    #select '20', :from => 'entry[calendar_date(3i)]'
+    select '2013', :from => 'entry[day(1i)]'
+    select 'July', :from => 'entry[day(2i)]'
+    select '20', :from => 'entry[day(3i)]'
     fill_in 'Event', :with => 'Dinner'
     fill_in 'Description', :with => "Dinner date with boyfriend"
     click_button 'Create Entry'
     page.should have_content('Entry has been created.')
+
+    entry = Entry.find_by_event("Dinner")
+    page.current_url.should == entry_url(entry)
+    title = "Dinner - Entries - Important Dates"
+    find("title").should have_content(title)
   end
 end
